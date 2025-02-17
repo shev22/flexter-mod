6b994b2e8c5689e87c9a752e791a9d90628544cb
francis <shevy2k2@yahoo.com>
2025-02-17 20:49:41 +0300

    feature/modify-actors-entity-and-filter-options

--- app/Actor/Http/Controllers/ActorsController.php
+++ app/Actor/Http/Controllers/ActorsController.php
@@ -4,13 +4,27 @@ namespace App\Actor\Http\Controllers;
 
 
 use App\Actor\Http\Request\ActorsFilterRequest;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use App\Actor\Repositories\Interfaces\ActorsRepositoryInterface;
 use App\Http\Controllers\Controller;
 
 class ActorsController extends Controller
 {
-    public function __invoke(ActorsFilterRequest $request): array
+    public function __construct(private readonly ActorsRepositoryInterface $actorsRepository)
     {
-        return [];
+    }
+
+    public function __invoke(ActorsFilterRequest $request): LengthAwarePaginator
+    {
+        try {
+            $search = $request->get('search');
+            $page = $request->get('page', 1);
+            $perPage = $request->get('perPage', 10);
+
+            return $this->actorsRepository->actors($search, $page, $perPage);
+        }catch (\Exception $exception){
+            echo $exception;
+        }
     }
 
 }
